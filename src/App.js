bd8e7bcffeee720f8580c427d90f8ac274137114
moaible <moai.motodera@gmail.com>
2020-10-06 04:00:52 +0000

    Change example text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Prototype example
         </a>
       </header>
     </div>
